cf7870e804d9c6fa38d2daa35246453f7a2ae4f6
Ovishake97 <71806046+Ovishake97@users.noreply.github.com>
2020-10-07 17:41:14 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,12 @@ namespace UserRegistrationProblem
             Console.WriteLine("Enter the lastname");
             string lastName = Console.ReadLine();
             validateUser.ValidateName(firstName,lastName);
+            string email = Console.ReadLine();
+            validateUser.ValidateEmail(email);
+            string phoneNo =Console.ReadLine();
+            validateUser.ValidateMobileNumber(phoneNo);
+            string passWord = Console.ReadLine();
+            validateUser.ValidatePassWord(passWord);
             Console.ReadKey();
         }
     }
@@ -45,6 +51,43 @@ namespace UserRegistrationProblem
                 Console.WriteLine("Invalid firstname and lastname");
             }
         }
+        
+        //Method to validate the email id
+        public void ValidateEmail(string email) {
+            Regex reg = new Regex("^[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$");
+            if (reg.IsMatch(email))
+            {
+                Console.WriteLine("Valid emailid");
+            }
+            else {
+                Console.WriteLine("Invalid emailid");
+            }
+        }
+
+        //Method to validate the mobile number
+        public void ValidateMobileNumber(string phoneNo) {
+            Regex rgx = new Regex("^[0-9]{2}[ ][0-9]{10}");
+            if (rgx.IsMatch(phoneNo))
+            {
+                Console.WriteLine("Valid mobile number");
+            }
+            else {
+                Console.WriteLine("Invalid mobile number");
+            }
+        }
+
+        //Method to validate password
+        public void ValidatePassword(string passWord) {
+            Regex rx = new Regex("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$");
+            if (rx.IsMatch(passWord))
+            {
+                Console.WriteLine("Valid password");
+            }
+            else {
+                Console.WriteLine("Invalid password");
+            }
+        }
+
 
     }
 }
